a37ff4b406e2ac95e61275ebfc834f85ee1c660e
fuzibotond <62881017+fuzibotond@users.noreply.github.com>
2021-10-07 18:09:36 +0300

    Labor 3 Done

--- AndroidQuiz/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ AndroidQuiz/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MyActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputTextArea = findViewById<EditText>(R.id.editTextTextPersonName)
+        val buttonGetStarted = findViewById<Button>(R.id.buttonGetStarted)
+        buttonGetStarted.setOnClickListener {
+            Log.i(TAG, "Button Clicked!")
+//            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+            val snackbar = Snackbar.make(
+                it,
+                "Button clicked! Thanks: ${inputTextArea.text.toString()}!",
+                Snackbar.LENGTH_SHORT
+            )
+            snackbar.show()
+        }
     }
+
+
+
 }
