5f250523521e87ff6f52f71ee4c193fcee04a2b9
fuzibotond <62881017+fuzibotond@users.noreply.github.com>
2021-10-27 10:56:02 +0200

    Labor 6 in progress

--- AndroidQuiz/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ AndroidQuiz/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
@@ -15,22 +16,48 @@ import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.quizapp.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() { //,Communicator
     private val TAG = "MainActivity"
+    private lateinit var topAppBar:MaterialToolbar
+    private lateinit var drawerLayout:DrawerLayout
+    private lateinit var navigationView:NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+        initializeView()
+        initMenu()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+    private fun initializeView(){
+        topAppBar = findViewById(R.id.topAppBar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.menu_navigation_view)
+    }
+    private fun initMenu(){
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
 
+        }
 
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            when(menuItem.itemId){
+                R.id.home -> findNavController(R.id.nav_host_fragment).navigate(R.id.quizStartFragment)
+            }
+            drawerLayout.close()
+            true
+        }
+    }
 
 
     override fun onPause() {
